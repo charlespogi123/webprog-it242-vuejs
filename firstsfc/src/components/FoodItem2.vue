c02d4fac8a6b082d7f4185e2973007f7d556af07
Charles B. Capagngan <cbcapagngan2@student.apc.edu.ph>
2026-01-16 10:20:14 +0800

    changes

---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div  class="durian">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Durian and Mangosteen',
+      message: 'I like durian and mangosteen'
+    }
+  }
+};
+</script>
+
+<style>
+  .durian {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: rgb(207, 227, 56);
+  }  
+</style>
